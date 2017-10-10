1db7c944caae87485eb61882235d211582b076cc
Javier Martinez <me@javierm.com>
2017-10-10 21:39:27 +1300

    enable cors on node api

--- node/app.js
+++ node/app.js
@@ -7,6 +7,15 @@ var client = new elasticsearch.Client({
 	log: "trace"
 });
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept"
+	);
+	next();
+});
+
 app.get("/transactions/:from/:size", function(req, res) {
 	client
 		.search({
